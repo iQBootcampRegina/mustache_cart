a1a3f73913ee1adfae39caf5634e7b436dcee963
Tiernan Hubble <thubble@gmail.com>
2015-01-14 13:34:06 -0600

    Add project

---
+++ CartService/CartModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace CartService
+{
+	public class CartModule : NancyModule
+	{
+		public CartModule()
+		{
+			Get["/"] = x => "HEY IT'S A CART SERVICE!! YAY!!!!";
+		}
+	}
+}
